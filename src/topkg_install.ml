dbuenzli/topkg

commit 76cc5845760aa9b090480d5f706a626617a46673

Finalize and document the behaviour of exec_field.
The magic done by ~auto:true allows us to drop the earlier
undocumented magic performed on moves with `Exe exts.

src/topkg_install.ml
@@ -41,20 +41,13 @@ let to_build ?header c os i =
   let native = Topkg_conf.OCaml.native ocaml_conf in
   let native_dylink = Topkg_conf.OCaml.native_dynlink ocaml_conf in
   let ext_to_string = Topkg_fexts.ext_to_string ocaml_conf in
+  let file_to_str (n, ext) = Topkg_string.strf "%s%s" n (ext_to_string ext) in
   let maybe_build = [ ".cmti"; ".cmt" ] in
   let bin_drops = List.map ext_to_string (bin_drop_exts native) in
   let lib_drops = List.map ext_to_string (lib_drop_exts native native_dylink) in
-  let file_to_str ?(build_target = false) (n, ext) =
-    let ext = match ext with
-    (* Work around https://github.com/ocaml/ocamlbuild/issues/6 *)
-    | `Exe when build_target -> `Ext ""
-    | _ -> ext
-    in
-    Topkg_string.strf "%s%s" n (ext_to_string ext)
-  in
   let add acc m =
     let mv (targets, moves) src dst =
-      let src = file_to_str ~build_target:true src in
+      let src = file_to_str src in
       let drop = not m.force && match m.field with
       | `Bin -> List.exists (Filename.check_suffix src) bin_drops
       | `Lib -> List.exists (Filename.check_suffix src) lib_drops
@@ -68,13 +61,13 @@ let to_build ?header c os i =
       let move = (m.field, Topkg_opam.Install.move ~maybe src ~dst) in
       (targets, move :: moves)
     in
-    let src =
-      if m.auto_bin && m.field = `Bin
-      then (if native then m.src ^ ".native" else m.src ^ ".byte")
-      else m.src
+    let src, dst =
+      if not m.auto_bin then m.src, m.dst else
+      ((if native then m.src ^ ".native" else m.src ^ ".byte"),
+       m.dst ^ (ext_to_string `Exe))
     in
-    if m.exts = [] then mv acc (split_ext src) (split_ext m.dst) else
-    let expand acc ext = mv acc (src, ext) (m.dst, ext) in
+    if m.exts = [] then mv acc (split_ext src) (split_ext dst) else
+    let expand acc ext = mv acc (src, ext) (dst, ext) in
     List.fold_left expand acc m.exts
   in
   let targets, moves = List.fold_left add ([], []) (flatten i) in
@@ -99,10 +92,8 @@ let _field field
   [{ field; auto_bin = auto; force; built; exts; src; dst }]
 
 let field field = _field ~auto:true field
-let field_exec
-    field ?auto ?force ?built ?cond ?(exts = Topkg_fexts.exe) ?dst src
-  =
-  _field field ?auto ?force ?built ?cond ~exts ?dst src
+let field_exec field ?auto ?force ?built ?cond ?exts ?dst src =
+  _field field ?auto ?force ?built ?cond ?exts ?dst src
 
 let bin = field_exec `Bin
 let doc = field `Doc

src/topkg.mli
@@ -962,12 +962,16 @@ Pkg.lib ~cond:has_jsoo ~exts:Exts.module_library "src/mylib_jsoo"
          {{!Conf.OCaml.native}native code compilation}}} *)
 
   type exec_field = ?auto:bool -> field
-  (** The type for field that install.
-
-      TODO reword this and make more precise. If [auto] is [true],
-      selects the native binary over the byte code one according to
-      the value of {!Conf.OCaml.native} and adds {!Exts.exe} to the
-      destination (which does the right thing on Windows). *)
+  (** The type for fields that install executable files. This is like {!field}
+      except for the additional [auto] parameter:
+      {ul
+      {- If [auto] is [true] (default) then [field src dst] will automatically
+         add the [".native"] suffix to [src] if {!Conf.OCaml.native}
+         is [true] and the [".byte"] suffix otherwise. Besides it will
+         automatically add {!Exts.exe} to [dst], which handles things
+         correctly on the various Windows ports.}
+      {- If [auto] is [false] you have full control according to
+         {!field}.}} *)
 
   val bin : exec_field
   (** [bin] is a field that installs to a common [bin/] directory. *)

commit ee11c9d869b768dea46cd1efbf245469a85fd329

Fix regression introduced by 76cc5845760aa9b09.

src/topkg_install.ml
@@ -91,7 +91,7 @@ let _field field
   in
   [{ field; auto_bin = auto; force; built; exts; src; dst }]
 
-let field field = _field ~auto:true field
+let field field = _field ~auto:false field
 let field_exec field ?auto ?force ?built ?cond ?exts ?dst src =
   _field field ?auto ?force ?built ?cond ?exts ?dst src
 