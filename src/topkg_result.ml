9b57ecbedefc46b9b89d1d65f132749d45b385df
Daniel Bünzli <daniel.buenzli@erratique.ch>
2025-03-10 16:55:28 +0100

    Remove some watermarks.

--- src/topkg_result.ml
+++ src/topkg_result.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2016 Daniel C. Bünzli. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 let ( >>= ) v f = match v with Ok v -> f v | Error _ as e -> e
